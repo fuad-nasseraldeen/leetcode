d165b91230d5b7d2a218ba83d3522f1e8ccb402c
github <fuads@LAPTOP-37UIH4DN>
2020-10-09 13:39:00 +0300

    Upload LongestCommonPrefix Question's

---
+++ leetCode/src/leetCode/lengthOfLastWord.java
@@ -0,0 +1,21 @@
+package leetCode;
+
+public class lengthOfLastWord {
+
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+        for (int i = s.length()-1; i>=0; i--){
+            if (s.charAt(i) == ' '){
+                if (length != 0){
+                    return length;                    
+                }
+            }
+            else{
+                length++;
+            }
+        }
+        
+        return length;
+    }
+
+}
